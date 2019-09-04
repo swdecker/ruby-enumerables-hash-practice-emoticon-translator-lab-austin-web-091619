5129fbfb80ad1c4c4cac8d013e2cc14b5f32096c
Steven Decker <swdecker@gmail.com>
2019-09-04 23:42:08 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,8 +18,13 @@ end
 
 def get_japanese_emoticon(file, emoticon)
   # code goes here
+  library = load_library(file)
+  library.find(emoticon) do 
+    
+  end
 end
 
 def get_english_meaning(file, emoticon)
   # code goes here
+  library = load_library(file)
 end
