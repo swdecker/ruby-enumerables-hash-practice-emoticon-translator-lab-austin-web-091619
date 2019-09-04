1d59e030f177d0f72e40480c82da778b9c1a5d3f
Steven Decker <swdecker@gmail.com>
2019-09-04 23:38:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,12 +6,12 @@ def load_library(file_name)
   # code goes here
   library = YAML.load_file("#{file_name}")
   #get_meaning is a japanese emote pointing to a description
-  #get_emoticon is the american emoticon ponting to a description
+  #get_emoticon is the american emoticon pointing to a japanese emoticon
   #binding pry
   organized_lib = {get_meaning: {}, get_emoticon: {}}
   library.each_key do |key|
     organized_lib[:get_meaning][library[key][1]]= key
-    organized_lib[:get_emoticon][library[key][0]] = key
+    organized_lib[:get_emoticon][library[key][0]] = library[key][1]
   end
   organized_lib
 end
